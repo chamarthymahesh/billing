583fd1d0e50fb88aae68f9cbcf583a2fc7378dea
chamarthy.mahesh <chamarthy.mahesh@gmail.com>
2026-05-18 09:50:21 +0530

    some errors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Request Logging
 app.use((req, res, next) => {
